68c298210acece29cf116759e469aaac08bdaee7
anjing <anjing@leadeon.cn>
2018-11-23 17:44:21 +0800

    三个页面样式over

---
+++ goods-vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <goodsdec></goodsdec>
+  </div>
+</template>
+
+<script>
+import goodslist from "./page/goodslist"
+import goodsins from "./page/goodsins"
+import goodsdec from "./page/goodsdec"
+export default {
+  name: 'App',
+  components:{
+    goodslist,
+    goodsins,
+    goodsdec
+  }
+}
+</script <style lang="scss" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
